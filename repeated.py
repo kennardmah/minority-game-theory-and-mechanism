29e3f32d1fb8f132cd9a71e7dbd8405bf89c6f81
Ken <73114932+kennardmah@users.noreply.github.com>
2024-02-17 17:29:02 +0000

    sim

--- repeated.py
+++ repeated.py
@@ -21,19 +21,16 @@ d = '000' # short term memory
 # initialise strategySet (strategy : value)
 # e.g., strategy = 000 if last three rounds were >50, >50, >50
 strategySet = {x: 0 for x in range(256)}
-print(strategySet)
 # initialise agents (agents : [strategy set])
-agents = {agent: [random.randint(0, 255) for _ in range(10)] for agent in range(1, 102)}
-print(agents)
+agents = {agent: [random.randint(0, 255) for _ in range(15)] for agent in range(1, 102)}
 # strategy_to_actions (strategy num: all potential history -> outcome)
 strategy_to_actions = generate_strategies()
-print(strategy_to_actions)
 # graph count
 count = []
 
 # RUN SIMULATION
 
-for i in range(10):
+for i in range(300):
     farol = 0
     house = 0
     for x, strats in agents.items():
@@ -53,7 +50,6 @@ for i in range(10):
         if action == 1:
             farol += 1
         else: house += 1
-
     if farol < house:
         minority = 1
     else: minority = 0
@@ -64,19 +60,20 @@ for i in range(10):
             strategySet[s] += 1
 
     # update memory
-    print(farol, house) 
     if farol < house:
-        d = d[1:] + '1'
-    else: d = d[1:] + '0'
+        d = d[1:] + '1' # farol is minority
+    else: d = d[1:] + '0' # home is minority
     count.append(farol)
+
 
 # Plotting
 plt.figure(figsize=(10, 6))  # Set the figure size (optional)
-plt.plot(list(range(len(count))), count, label='Farol Value', marker='o', linestyle='-')
+plt.plot(list(range(len(count))), count, label='Count', marker='o', linestyle='-')
 plt.axhline(y=50, color='r', linestyle='--', label='Y=50')  # Add horizontal line at y=50
 plt.title('How busy is El Farol?')
 plt.xlabel('Week')
 plt.ylabel('Bar Count')
 plt.legend()
+plt.ylim(0, 100)
 plt.grid(True)
 plt.show()
